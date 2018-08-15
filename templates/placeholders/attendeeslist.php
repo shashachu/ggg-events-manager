1bc113fa2c4ff179aca9e65e45e37e541f826856
Sha Sha Chu <shashachu@gmail.com>
2018-08-14 23:05:51 -0700

    Implemented two features: * Group RSVPs by ticket type and also sort by costume type and name * Copy email to clipboard for ERs
    Closes #1 and closes #5

--- templates/placeholders/attendeeslist.php
+++ templates/placeholders/attendeeslist.php
@@ -6,25 +6,84 @@ if( count($EM_Bookings->bookings) > 0 ){
 	?>
 	<ul class="event-attendees">
 	<?php
+	$bookings_by_ticket = array();
 	foreach( $EM_Bookings as $EM_Booking){ /* @var $EM_Booking EM_Booking */
 		if($EM_Booking->booking_status == 1 && !in_array($EM_Booking->get_person()->ID, $people) ){
 			$people[] = $EM_Booking->get_person()->ID;
 			$ticket_bookings = $EM_Booking->get_tickets_bookings();
 			$ticket_type = $EM_Booking->booking_comment;
-			if (empty($ticket_type) && count($ticket_bookings) > 0) {
-				foreach ($EM_Booking->get_tickets_bookings()->tickets_bookings as $EM_Ticket_Booking) {
-					if ($EM_Ticket_Booking->get_spaces() > 0) {
-						$ticket_type = EM_Ticket::get_ticket_name($EM_Ticket_Booking->ticket_id);
+			foreach ($EM_Booking->get_tickets_bookings()->tickets_bookings as $EM_Ticket_Booking) {
+				if ($EM_Ticket_Booking->get_spaces() > 0) {
+					$ticket_name = EM_Ticket::get_ticket_name($EM_Ticket_Booking->ticket_id);
+					if (empty($ticket_type) && count($ticket_bookings) > 0) {
+						$ticket_type = $ticket_name;
 						break;
 					}
 				}
 			}
-			echo '<li>'. $EM_Booking->get_person()->get_name() . ' - ' . $ticket_type .'</li>';
-		}elseif($EM_Booking->booking_status == 1 && $EM_Booking->is_no_user() ){
-			echo '<li>'. $EM_Booking->get_person()->get_name() .'</li>';
+			$bookings_by_ticket[$ticket_name][] = array('name' => $EM_Booking->get_person()->get_name(),
+				'ticket_type' => $ticket_type,
+				'email' => $EM_Booking->person->user_email);
+		}
+	}
+
+	foreach ($bookings_by_ticket as $ticket_name => $bookings) {
+		echo '<h4>' . $ticket_name . '</h4>';
+		usort($bookings, function($a, $b) {
+			$cmp = strcmp($a['ticket_type'], $b['ticket_type']);
+			if ($cmp == 0) {
+				return strcmp($a['name'], $b['name']);
+			} else {
+				return $cmp;
+			}
+		});
+		foreach ($bookings as $booking) {
+			echo '<li>'. $booking['name'] . ' - ' . $booking['ticket_type'] . '</li>';
 		}
 	}
 	?>
 	</ul>
+
+	<div id='emails' style='display:none'>
+		<?php
+			$firstElem = true;
+			foreach ($bookings_by_ticket as $ticket_name => $bookings) {
+				foreach ($bookings as $booking) {
+					if (!$firstElem) {
+						echo ',';
+					} else {
+						$firstElem = false;
+					}
+					echo $booking['email'];
+				}
+			}
+		?>
+	</div>
+
+	<script type="text/javascript">
+		function selectText(containerid) {
+			if (document.selection) { // IE
+				var range = document.body.createTextRange();
+				range.moveToElementText(document.getElementById(containerid));
+				range.select();
+			} else if (window.getSelection) {
+				var range = document.createRange();
+				range.selectNode(document.getElementById(containerid));
+				window.getSelection().removeAllRanges();
+				window.getSelection().addRange(range);
+			}
+		}
+
+		function copyEmails() {
+			var emailsDiv = document.getElementById("emails");
+			emailsDiv.style.display = "block";
+			selectText("emails");
+			document.execCommand("copy");
+			emailsDiv.style.display = "none";
+		}
+	</script>
+
+	<button onclick="copyEmails()">Copy emails to clipboard</button>
+
 	<?php
 }
